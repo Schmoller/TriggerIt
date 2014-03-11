8ef3a75b3d158c146d383968d59caf02ade7a374
Schmoller <steven.schmoll@gmail.com>
2014-03-11 18:50:59 +1100

    BlockTrigger should now be complete (except save/load)

--- src/au/com/addstar/triggerit/triggers/BlockTrigger.java
+++ src/au/com/addstar/triggerit/triggers/BlockTrigger.java
@@ -1,16 +1,20 @@
 package au.com.addstar.triggerit.triggers;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.WeakHashMap;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -36,6 +40,24 @@ public class BlockTrigger extends Trigger
 		RightClick,
 		Physical,
 		BlockUpdate
+	}
+	
+	private static ArrayList<String> mTriggerTypeNames;
+	private static HashMap<String, TriggerType> mTriggerTypeMap;
+	
+	// Triggers waiting to be completed
+	private static WeakHashMap<Player, BlockTrigger> mWaitingTriggers = new WeakHashMap<Player, BlockTrigger>();
+	
+	static
+	{
+		mTriggerTypeNames = new ArrayList<String>();
+		mTriggerTypeMap = new HashMap<String, TriggerType>();
+		
+		for(TriggerType type : TriggerType.values())
+		{
+			mTriggerTypeNames.add(type.name());
+			mTriggerTypeMap.put(type.name().toLowerCase(), type);
+		}
 	}
 	
 	// Order: World, X, Z, Y
@@ -66,62 +88,62 @@ public class BlockTrigger extends Trigger
 		return zMap.get(y);
 	}
 	
-	private static void removeTriggerAt(World world, int x, int y, int z, BlockTrigger trigger)
+	private static void removeTriggerAt(UUID world, BlockVector location, BlockTrigger trigger)
 	{
-		HashMap<Integer, HashMap<Integer, HashMultimap<Integer, BlockTrigger>>> worldMap = mBlockTriggers.get(world.getUID());
+		HashMap<Integer, HashMap<Integer, HashMultimap<Integer, BlockTrigger>>> worldMap = mBlockTriggers.get(world);
 		
 		if(worldMap == null)
 			return;
 		
-		HashMap<Integer, HashMultimap<Integer, BlockTrigger>> xMap = worldMap.get(x);
+		HashMap<Integer, HashMultimap<Integer, BlockTrigger>> xMap = worldMap.get(location.getBlockX());
 		
 		if(xMap == null)
 			return;
 		
-		HashMultimap<Integer, BlockTrigger> zMap = xMap.get(z);
+		HashMultimap<Integer, BlockTrigger> zMap = xMap.get(location.getBlockZ());
 		
 		if(zMap == null)
 			return;
 		
-		zMap.remove(y, trigger);
+		zMap.remove(location.getBlockY(), trigger);
 		
 		if(zMap.isEmpty())
-			xMap.remove(z);
+			xMap.remove(location.getBlockZ());
 		
 		if(xMap.isEmpty())
-			worldMap.remove(x);
+			worldMap.remove(location.getBlockX());
 		
 		if(worldMap.isEmpty())
-			mBlockTriggers.remove(world.getUID());
+			mBlockTriggers.remove(world);
 	}
 	
-	private static void addTriggerAt(World world, int x, int y, int z, BlockTrigger trigger)
+	private static void addTriggerAt(UUID world, BlockVector location, BlockTrigger trigger)
 	{
-		HashMap<Integer, HashMap<Integer, HashMultimap<Integer, BlockTrigger>>> worldMap = mBlockTriggers.get(world.getUID());
+		HashMap<Integer, HashMap<Integer, HashMultimap<Integer, BlockTrigger>>> worldMap = mBlockTriggers.get(world);
 		
 		if(worldMap == null)
 		{
 			worldMap = new HashMap<Integer, HashMap<Integer,HashMultimap<Integer,BlockTrigger>>>();
-			mBlockTriggers.put(world.getUID(), worldMap);
+			mBlockTriggers.put(world, worldMap);
 		}
 		
-		HashMap<Integer, HashMultimap<Integer, BlockTrigger>> xMap = worldMap.get(x);
+		HashMap<Integer, HashMultimap<Integer, BlockTrigger>> xMap = worldMap.get(location.getBlockX());
 		
 		if(xMap == null)
 		{
 			xMap = new HashMap<Integer, HashMultimap<Integer,BlockTrigger>>();
-			worldMap.put(x, xMap);
+			worldMap.put(location.getBlockX(), xMap);
 		}
 		
-		HashMultimap<Integer, BlockTrigger> zMap = xMap.get(z);
+		HashMultimap<Integer, BlockTrigger> zMap = xMap.get(location.getBlockZ());
 		
 		if(zMap == null)
 		{
 			zMap = HashMultimap.create();
-			xMap.put(z, zMap);
+			xMap.put(location.getBlockZ(), zMap);
 		}
 		
-		zMap.put(y, trigger);
+		zMap.put(location.getBlockY(), trigger);
 	}
 	
 	private TriggerType mType;
@@ -142,15 +164,54 @@ public class BlockTrigger extends Trigger
 		return mType;
 	}
 	
+	public void setType(TriggerType type)
+	{
+		mType = type;
+	}
+	
+	private void setBlock(Block block)
+	{
+		mWorld = block.getWorld().getUID();
+		mLocation = new BlockVector(block.getX(), block.getY(), block.getZ());
+	}
+	
 	@Override
 	public boolean isValid()
 	{
-		return false;
+		return mWorld != null && mLocation != null;
 	}
 
+	@Override
+	public void onLoad()
+	{
+		addTriggerAt(mWorld, mLocation, this);
+	}
+	
+	@Override
+	public void onUnload()
+	{
+		removeTriggerAt(mWorld, mLocation, this);
+	}
+	
 	public static BlockTrigger newTrigger(CommandSender sender, String[] args) throws IllegalArgumentException, IllegalStateException
 	{
-		return null;
+		if(!(sender instanceof Player))
+			throw new IllegalStateException("Block is only usable by players.");
+		
+		if(args.length != 1)
+			throw new IllegalStateException("<type>");
+
+		TriggerType type = mTriggerTypeMap.get(args[0].toLowerCase());
+		if(type == null)
+			throw new IllegalArgumentException("Unknown block trigger type " + args[0]);
+		
+		BlockTrigger trigger = new BlockTrigger();
+		trigger.setType(type);
+		
+		mWaitingTriggers.put((Player)sender, trigger);
+		sender.sendMessage(ChatColor.GREEN + "Please click a block to complete the block trigger");
+		
+		return trigger;
 	}
 	
 	public static List<String> tabComplete(CommandSender sender, String[] args)
@@ -172,6 +233,9 @@ public class BlockTrigger extends Trigger
 			
 			for(BlockTrigger trigger : triggers)
 			{
+				if(!trigger.isEnabled())
+					continue;
+				
 				if(trigger.getType() == TriggerType.Place)
 					trigger.trigger(event.getPlayer());
 			}
@@ -184,6 +248,9 @@ public class BlockTrigger extends Trigger
 			
 			for(BlockTrigger trigger : triggers)
 			{
+				if(!trigger.isEnabled())
+					continue;
+				
 				if(trigger.getType() == TriggerType.Remove)
 					trigger.trigger(event.getPlayer());
 			}
@@ -196,6 +263,9 @@ public class BlockTrigger extends Trigger
 			
 			for(BlockTrigger trigger : triggers)
 			{
+				if(!trigger.isEnabled())
+					continue;
+				
 				if(trigger.getType() == TriggerType.BlockUpdate)
 					trigger.trigger(null);
 			}
@@ -211,12 +281,33 @@ public class BlockTrigger extends Trigger
 			
 			for(BlockTrigger trigger : triggers)
 			{
+				if(!trigger.isEnabled())
+					continue;
+				
 				if(trigger.getType() == TriggerType.LeftClick && event.getAction() == Action.LEFT_CLICK_BLOCK)
 					trigger.trigger(event.getPlayer());
 				else if(trigger.getType() == TriggerType.RightClick && event.getAction() == Action.RIGHT_CLICK_BLOCK)
 					trigger.trigger(event.getPlayer());
 				else if(trigger.getType() == TriggerType.Physical && event.getAction() == Action.PHYSICAL)
 					trigger.trigger(event.getPlayer());
+			}
+		}
+		
+		@EventHandler(priority=EventPriority.LOWEST, ignoreCancelled=false)
+		public void onBlockClickLowest(PlayerInteractEvent event)
+		{
+			if(event.getAction() != Action.LEFT_CLICK_BLOCK && event.getAction() != Action.RIGHT_CLICK_BLOCK)
+				return;
+			
+			BlockTrigger trigger = mWaitingTriggers.remove(event.getPlayer());
+			
+			if(trigger != null)
+			{
+				Block block = event.getClickedBlock();
+				trigger.setBlock(block);
+				TriggerItPlugin.getInstance().getTriggerManager().completeTrigger(trigger);
+				event.getPlayer().sendMessage(ChatColor.GREEN + "Finished creation of Block trigger at " + String.format("%d, %d, %d in %s", block.getX(), block.getY(), block.getZ(), block.getWorld().getName()));
+				event.setCancelled(true);
 			}
 		}
 	}
